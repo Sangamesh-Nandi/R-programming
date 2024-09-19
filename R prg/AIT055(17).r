b8a61a03b5cd7d8b8cd7422e8a1b303b3201f2e2
Sangamesh Nandi <sangameshv.24.beai@acharya.ac.in>
2024-09-19 22:54:53 +0530

    Update AIT055(17).r

--- R prg/AIT055(17).r
+++ R prg/AIT055(17).r
@@ -1,2 +1,7 @@
-list = list(string_value = "Hello acharya",number_value =10 ,vector_value = c(1, 2, 3, 4, 5),logical_value = TRUE)
-print(list)
+my_list <- list(
+  string_value = "Hello","Acharya",
+  numeric_value = c(42,22,54),
+  vector_value = c(1, 2+3i, 3L, "r", "Sangamesh"),
+  logical_value = c(TRUE, FALSE) 
+)
+print(my_list)
